39ef7786fab06a603d9278f8ba0ca316225e503b
bpiotrowski <baej.p26@gmail.com>
2019-07-26 12:36:35 +0200

    added sql query

--- src/main/java/pl/bpiotrowski/OracleConnection.java
+++ src/main/java/pl/bpiotrowski/OracleConnection.java
@@ -5,15 +5,21 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class OracleConnection {
-    public void connect() {
+class OracleConnection {
+    void connect() {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "oraclexe");
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from apw_user.DEDP");
-            while (rs.next()) {
-                System.out.println(rs.getInt(1) + "  " + rs.getString(3));
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT df.tablespace_name \"Tablespace\", df.bytes / (1024 * 1024)" +
+                    " \"Size(MB)\", SUM(fs.bytes) / (1024 * 1024) \"Free(MB)\", Nvl(Round(SUM(fs.bytes) * 100 / df.bytes),1)" +
+                    " \"% Free\", Round((df.bytes - SUM(fs.bytes)) * 100 / df.bytes) \"% Used\" FROM dba_free_space fs, " +
+                    "(SELECT tablespace_name,SUM(bytes) bytes FROM dba_data_files GROUP BY tablespace_name) df " +
+                    "WHERE fs.tablespace_name (+)  = df.tablespace_name and df.tablespace_name in ('APWDATA','EDE','KS','USERS','APWIDX') " +
+                    "GROUP BY df.tablespace_name,df.bytes");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + " " + resultSet.getInt(2) + " " + resultSet.getDouble(3));
             }
             con.close();
         } catch (Exception e) {
